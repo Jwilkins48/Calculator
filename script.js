9e32f88b26004309dff43932b0cceafe30dde93d
Jenna <jenna48071@gmail.com>
2021-12-17 15:20:31 -0600

    added functioning for buttons

--- script.js
+++ script.js
@@ -6,21 +6,57 @@ const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('subtract');
 const multiplyBtn = document.getElementById('multiply');
 const divideBtn = document.getElementById('divide');
-const digits = document.querySelectorAll('.digit');
-const operators = document.querySelectorAll('.operator');
+const digitsBtn = document.querySelectorAll('.digit');
+const operationBtn = document.querySelectorAll('.operator');
 
 let userDisplay = document.getElementById('userInput');
 let resultDisplay = document.getElementById('userResult');
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
 
 
 // Calculation functions
-let add = (num1, num2) => num1 + num2;
-let subtract = (num1, num2) => num1 - num2;
-let multiply = (num1, num2) => num1 * num2;
-let divide = (num1, num2) => num1 / num2;
+let add = (n1, n2) => n1 + n2;
+let subtract = (n1, n2) => n1 - n2;
+let multiply = (n1, n2) => n1 * n2;
+let divide = (n1, n2) => n1 / n2;
 
-digits.forEach(button =>{
-    button.addEventListener('click', () =>{
-        userDisplay.innerText = (button.innerText)
-    })
-})
+const Calculation = (n1, operation, n2) => {
+    switch (operation) {
+        case 'add':
+            return add(n1, n2);
+        case 'subtract':
+            return subtract(n1, n2);
+        case 'multiply':
+            return multiply(n1, n2);
+        case 'divide':
+            return divide(n1, n2);
+    };
+};
+
+// Number buttons
+digitsBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (operator === '') {
+            firstNumber += e.target.innerText;
+            userDisplay.innerText = firstNumber;
+        } else {
+            secondNumber += e.target.innerText;
+            userDisplay.innerText = `${firstNumber} ${operator} ${secondNumber}`
+        };
+    });
+});
+
+// operator buttons
+operationBtn.forEach(opr => {
+    opr.addEventListener('click', (e) => {
+        if (e.target.innerText !== '=') {
+            operator = e.target.innerText
+            userDisplay.innerText = `${firstNumber} ${operator}`
+        } else {
+            userDisplay.innerText = Calculation(firstNumber, operator, secondNumber);
+            
+        };
+    });
+});
